40026809b7517647bfb3bc6c8f5307270d66698e
nasimahmed0909 <nasimahemd0909@gmail.com>
2023-04-02 14:14:08 +0600

    first commit

---
+++ code.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+#define ll long long
+using namespace std;
+
+int main()
+{
+
+    int a, b, sum;
+    cin >> a >> b;
+    sum = a - b;
+    cout << sum<<endl;
+    
+    
+    return 0;
+}
---
+++ templete.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+#define ll long long
+#define pii pair<int, int>
+#define sz(v) v.size()
+#define mem(a, x) memset(a, x, sizeof(a))
+#define fio                       \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0)
+using namespace std;
+int main()
+{
+    fio;
+
+    return 0;
+}
